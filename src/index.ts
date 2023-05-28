bd1ce07694247e8b62e6dc4e1325ac8fc37c71f0
TJ <tjdrzewiecki@gmail.com>
2023-05-28 00:03:15 -0400

    Removing the separation of concerns until we get a gameplan together

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-(function gameboard() {
-  const house = document.querySelector("#house")!;
-  for (let i = 1; i <= 6; i++) {
-    const room = document.createElement("div");
-    room.className = i.toString();
-    house.append(room);
-  }
-})()
+const killerStart = document.querySelector("#killer-start");
+const playerStart = document.querySelector("#player-start");
+const house = document.querySelector("#house")!;
+
+for (let i = 1; i <= 6; i++) {
+  const room = document.createElement("div");
+  room.className = i.toString();
+  house.append(room);
+}
 
