f000552b0416c9c6953288f6423dcc5508f09dbd
TJ <tjdrzewiecki@gmail.com>
2023-05-28 13:18:56 -0400

    Valid player moves to adjacent rooms, while disallowing diagonals complete

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// The more columns there are, it will be harder for the player (longer to get to the top). The more rows there are, it will be harder for the killer (more guesses around to find player) 3 and 3 are a good, fair start to get a stable app
 const killerStart = document.querySelector("#killer-start")!;
 const playerStart = document.querySelector("#player-start")!;
 const house = document.querySelector("#house")!;
@@ -6,24 +7,28 @@ const player = document.querySelector("#player")!;
 
 const grid: number[][] = [];
 
-// The more columns there are, it will be harder for the player (longer to get to the top). The more rows there are, it will be harder for the killer (more guesses around to find player) 3 and 3 are a good, fair start to get a stable app
-
-for (let i = 0; i <= 2; i++) {
+for (let x = 0; x <= 2; x++) {
   grid.push([]);
-  for (let j = 0; j <= 2; j++) {
+  for (let y = 0; y <= 2; y++) {
     const room = document.createElement("div");
     room.className = "room";
-    room.addEventListener("click", () => moveCharacter(room));
+    room.addEventListener("click", () => moveCharacter(x, y, room));
     house.append(room);
-    grid[i].push(j);
+    grid[x].push(y);
   }
 }
-console.log(grid);
 
-function moveCharacter(room: HTMLDivElement) {
-  if (killer.parentElement === room) {
-    // lose game
-  } else {
+let killerPosition: number[] = [-1, 1];
+let playerPosition: number[] = [3, 1];
+
+function moveCharacter(x: number, y: number, room: HTMLDivElement) {
+  if (
+    (playerPosition[0] === x &&
+      (playerPosition[1] === y + 1 || playerPosition[1] === y - 1)) ||
+    (playerPosition[1] === y &&
+      (playerPosition[0] === x + 1 || playerPosition[0] === x - 1))
+  ) {
+    playerPosition = [x, y];
     player.parentElement?.removeChild(player);
     room.appendChild(player);
   }
