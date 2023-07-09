116d8054abc8982b6e378a1c7d200a942e14cbde
TJ <tjdrzewiecki@gmail.com>
2023-07-09 01:08:41 -0400

    Difficulty dialog created. Add multiple bugs. Class names may be off. Player not moving correctly. CSS issues. WIP

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,17 @@ import './styles/reset.css';
 import './styles/styles.css';
 import createHouse from './scripts/createHouse';
 
-createHouse();
+const startDialog = document.querySelector(
+  '#start-dialog',
+) as HTMLDialogElement;
+const difficultyForm = document.querySelector(
+  '#difficulty-form',
+) as HTMLFormElement;
+
+startDialog.showModal();
+difficultyForm.addEventListener('submit', () => {
+  const value = document.querySelector(
+    'input[name="difficulty"]:checked',
+  ) as HTMLInputElement;
+  createHouse(Number(value.value));
+});
--- src/scripts/createHouse.ts
+++ src/scripts/createHouse.ts
@@ -3,6 +3,12 @@ import movePlayer from './movePlayer';
 import moveKiller from './moveKiller';
 import gameCheck from './gameCheck';
 
+const startDialog = document.querySelector(
+  '#start-dialog',
+) as HTMLDialogElement;
+const difficultyForm = document.querySelector(
+  '#difficulty-form',
+) as HTMLFormElement;
 const house = document.querySelector('#house')!;
 const resultDialog = document.querySelector(
   '#result-dialog',
@@ -16,15 +22,21 @@ const grid: number[][] = [];
 const killerPosition: number[] = [];
 const playerPosition: number[] = [];
 
-export default function createHouse() {
+// easy(7x7), medium(5x5), or hard(3x3) pick
+// row for killer will be 0
+// row for player will be max length - 1
+// column for killer will be max length / 2 .floor
+// column for player will be max length / 2 .ceil (start a little opposite)
+
+export default function createHouse(difficulty: number) {
   killerPosition.push(0);
   killerPosition.push(1);
   playerPosition.push(2);
   playerPosition.push(1);
 
-  for (let x = 0; x <= 2; x += 1) {
+  for (let x = 0; x < difficulty; x += 1) {
     grid.push([]);
-    for (let y = 0; y <= 2; y += 1) {
+    for (let y = 0; y < difficulty; y += 1) {
       grid[x].push(y);
       const room = document.createElement('div');
       room.classList.add('room');
@@ -58,5 +70,11 @@ export default function createHouse() {
 resultDialog.addEventListener('submit', () => {
   resultDialog.classList.remove(...resultDialog.classList);
   deleteHouse(grid, killerPosition, playerPosition);
-  createHouse();
+  startDialog.showModal();
+  difficultyForm.addEventListener('submit', () => {
+    const value = document.querySelector(
+      'input[name="difficulty"]:checked',
+    ) as HTMLInputElement;
+    createHouse(Number(value.value));
+  });
 });
