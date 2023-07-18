d41598aff0a5d198215d6988ee013797b73aa9c2
TJ <tjdrzewiecki@gmail.com>
2023-07-17 20:59:29 -0400

    minimax considered and test ideas though of for smart killer AI

---
+++ src/tests/minimax.test.ts
@@ -0,0 +1,9 @@
+import minimax from '../scripts/minimax';
+
+test('Kills for win if possible', () => {});
+
+test('Moves toward the player instead of away', () => {});
+
+test('Stays in between exit and player', () => {});
+
+// do not allow killer to stay still. If player is cornered, it will be a stand still indefinitely
