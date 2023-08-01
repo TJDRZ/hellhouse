39ca6eda2567b8e6e038225c24c757f3b3a2864f
TJ <tjdrzewiecki@gmail.com>
2023-08-01 18:33:26 -0400

    WIP- smartKiller AI tdd; Moving towards player

--- src/scripts/smartKiller.ts
+++ src/scripts/smartKiller.ts
@@ -2,6 +2,7 @@ export default function smartKiller(
   killerPosition: number[],
   playerPosition: number[],
 ) {
+  // maybe make a switch statement if we have a ton of ifs that make sense / work in a switch
   if (
     // Same row and column +/- 1
     killerPosition[0] === playerPosition[0] &&
@@ -16,7 +17,25 @@ export default function smartKiller(
   ) {
     return playerPosition;
   }
-  return [];
+  if (killerPosition[0] === playerPosition[0]) {
+    // Same row, if column higher or lower then move in that direction
+    if (playerPosition[1] > killerPosition[1]) {
+      return [killerPosition[0], killerPosition[1] + 1];
+    }
+    return [killerPosition[0], killerPosition[1] - 1];
+  }
+  if (killerPosition[1] === playerPosition[1]) {
+    // Same column, if row higher or lower then move in that direction
+    if (playerPosition[0] > killerPosition[0]) {
+      return [killerPosition[0] + 1, killerPosition[1]];
+    }
+    return [killerPosition[0] - 1, killerPosition[1]];
+  }
+  // Prioritize moving rows towards player
+  if (playerPosition[0] > killerPosition[0]) {
+    return [killerPosition[0] + 1, killerPosition[1]];
+  }
+  return [killerPosition[0] - 1, killerPosition[1]];
 }
 
 /*
