1c0b512bbe07ab7e721acba47e40ec622e13319f
TJ <tjdrzewiecki@gmail.com>
2023-08-01 21:10:51 -0400

    WIP- smartKiller AI tdd; Moving towards player

--- src/tests/smartKiller.test.ts
+++ src/tests/smartKiller.test.ts
@@ -12,9 +12,13 @@ describe('Kills for win if possible', () => {
 });
 
 describe('Moves toward the player instead of away', () => {
-  test('Moves towards player', () => {
+  test('Moves towards player - down rows', () => {
     expect(smartKiller([1, 1], [2, 2])[0]).toEqual(2);
     expect(smartKiller([1, 1], [2, 2])[1]).toEqual(1);
+  });
+  test('Moves towards player - up rows', () => {
+    expect(smartKiller([1, 1], [0, 0])[0]).toEqual(0);
+    expect(smartKiller([1, 1], [0, 0])[1]).toEqual(1);
   });
 });
 
