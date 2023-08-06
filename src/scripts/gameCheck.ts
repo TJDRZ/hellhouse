4580ae7afeda50b15bb6c4ad7a7aa98071c0b3c5
TJ <tjdrzewiecki@gmail.com>
2023-08-06 13:06:18 -0400

    gameCheck testing and gameCheck.ts changed to use textContent as opposed to innerText

--- src/scripts/gameCheck.ts
+++ src/scripts/gameCheck.ts
@@ -9,19 +9,19 @@ export default function gameCheck(
   const resultDialogText = document.querySelector(
     '#result-dialog-text',
   ) as HTMLParagraphElement;
-  
+
   if (
     killerPosition[0] === playerPosition[0] &&
     killerPosition[1] === playerPosition[1]
   ) {
     resultDialog.classList.add('diedDialog');
     resultDialog.showModal();
-    resultDialogText.innerText = 'You Died!';
+    resultDialogText.textContent = 'You Died!';
   } else if (
     playerPosition[0] === escapeRoomPosition[0] &&
     playerPosition[1] === escapeRoomPosition[1]
   ) {
     resultDialog.showModal();
-    resultDialogText.innerText = 'You Escaped!';
+    resultDialogText.textContent = 'You Escaped!';
   }
 }
--- src/tests/gameCheck.test.ts
+++ src/tests/gameCheck.test.ts
@@ -1,10 +1,29 @@
 import gameCheck from '../scripts/gameCheck';
 
-document.body.innerHTML = '<div id="result-dialog"></div>';
+document.body.innerHTML =
+  '<dialog id="result-dialog"><p id="result-dialog-text"></p></dialog>';
+
 const resultDialog = document.querySelector(
   '#result-dialog',
 ) as HTMLDialogElement;
+const resultDialogText = document.querySelector(
+  '#result-dialog-text',
+) as HTMLParagraphElement;
 
-test('Designates a kill and the result-dialog displays properly', () => {});
+// HTML Dialog element does not have support on jsdom
+HTMLDialogElement.prototype.showModal = jest.fn(function mock(
+  this: HTMLDialogElement,
+) {
+  this.open = true;
+});
 
-test('Designates an escape and the result-dialog displays properly', () => {});
+test('Designates a kill and the result-dialog displays properly', () => {
+  gameCheck([0, 0], [0, 0], [2, 2]);
+  expect(resultDialog.classList.contains('diedDialog')).toBeTruthy();
+  expect(resultDialogText.textContent).toMatch('You Died!');
+});
+
+test('Designates an escape and the result-dialog displays properly', () => {
+  gameCheck([0, 0], [1, 1], [0, 0]);
+  expect(resultDialogText.textContent).toMatch('You Escaped!');
+});
